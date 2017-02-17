0edecb10805a7702984aa84487a75173c3c86b66
José Francisco Herrera Hernández <pako@MacBook-Pro-de-Jose.local>
2017-02-16 23:07:51 -0600

    created a controller to hide the ui-view

--- assignments/assignment4/src/menu/menuapp.module.js
+++ assignments/assignment4/src/menu/menuapp.module.js
@@ -3,4 +3,29 @@
 
 	angular.module('MenuApp', ['ui.router', 'data', 'Spinner']);
 
+	angular.module('MenuApp')
+		.controller('MenuAppController', MenuAppController);
+
+	MenuAppController.$inject = ['$rootScope'];
+	function MenuAppController ($rootScope) {
+		var menuCtrl = this;
+
+		menuCtrl.$onInit = function () {
+			$rootScope.$on('$stateChangeStart',
+			function(event, toState, toParams, fromState, fromParams, options){
+				menuCtrl.inTransition = true;
+			});
+
+			$rootScope.$on('$stateChangeSuccess',
+			function(event, toState, toParams, fromState, fromParams){
+				menuCtrl.inTransition = false;
+			});
+
+			$rootScope.$on('$stateChangeError',
+			function(event, toState, toParams, fromState, fromParams, error){
+				menuCtrl.inTransition = true;
+			});
+		}
+	}
+
 })();
