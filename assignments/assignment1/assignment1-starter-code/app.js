d45a8ea986ea10bd99f7e730c2129ad7a713eb6b
José Francisco Herrera Hernández <pako@MacBook-Pro-de-Jose.local>
2017-05-14 19:03:42 -0500

    The previous assignments where deleted with my previous commit, I'm uploading again

---
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,28 @@
+(function(){
+	'use strict';
+	angular.module('FirstAssignment', [])
+	.controller('FirstAssignmentCtrl', myController);
+
+	myController.$inject = ['$scope'];
+
+	function myController($scope){
+		$scope.userInput = "";
+
+		$scope.message = "";
+
+		$scope.checkInput = function checkInput(input){
+			if(input == ''){
+				$scope.message = 'Please enter data first';
+			}else{
+				var items = input.split(',');
+				if(items.length <= 3){
+					$scope.message = 'Enjoy!';
+				}else{
+					$scope.message = 'Too much!';
+				}
+			}
+			//$scope.message= input;
+		}
+	}
+
+})();
